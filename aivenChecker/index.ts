1ce9249876a872cae6a0d98f1cfb328bea76391e
Michus3142 <mstrojek8@gmail.com>
2025-06-02 21:11:26 +0200

    Update: Should be working if database goes online

--- aivenChecker/index.ts
+++ aivenChecker/index.ts
@@ -15,11 +15,43 @@ if (!process.env.DATABASE_URL) {
 const sql = postgres(process.env.DATABASE_URL);
 
 setInterval(async () => {
-    try {
+  try {
     const test = await sql`SELECT 1`;
     console.log("Database connection successful:", test);
 
     if (!state) {
+      const port = process.env.PORT || 3000;
+
+      const payload: payload = {
+        application: "Aiven",
+        type: "Info",
+        message: "Database connection successful",
+        timestamp: new Date().toISOString(),
+      };
+
+      try {
+        await fetch(`http://localhost:${port}/health`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } catch (error) {
+        console.error(
+          "Health check failed at: http://localhost:${port}/health at",
+          new Date().toISOString()
+        );
+        return;
+      }
+
+      const response = await fetch(`http://localhost:${port}/send`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ payload: payload }),
+      });
+
       state = true;
     }
   } catch (error) {
@@ -57,4 +89,4 @@ setInterval(async () => {
       });
     }
   }
-}, 60000)
+}, 60000);
